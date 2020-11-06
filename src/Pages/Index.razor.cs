b61c29d43fe42e80bd5fbd64e1250a66436b6c96
James Yeung <shunjiey@hotmail.com>
2020-11-06 23:49:19 +0800

    feat: add countdown timer

---
+++ src/Pages/Index.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace DotNetConf.Web.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        private TimeSpan countDown;
+        private static readonly Timer timer = new Timer();
+
+        protected override Task OnInitializedAsync()
+        {
+            Refresh();
+
+            timer.Elapsed += Refresh;
+            timer.Interval = 500;
+            timer.Start();
+
+            return Task.CompletedTask;
+        }
+
+        private void Refresh(object sender = default, ElapsedEventArgs e = default)
+        {
+            countDown = new DateTime(2020, 12, 19) - DateTime.Now;
+            InvokeAsync(StateHasChanged);
+        }
+    }
+}
