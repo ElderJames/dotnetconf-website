0d6429b0c0a8d6ab6bdb569f86c2bcf593df8232
James Yeung <shunjiey@hotmail.com>
2020-12-01 22:28:03 +0800

    add photos

--- src/Program.cs
+++ src/Program.cs
@@ -15,6 +15,7 @@ namespace DotNetConf.Web
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddAntDesign();
 
             CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.CurrentCulture;
 
